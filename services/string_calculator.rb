be67799c6e38def9e197d2c6849e683ae8d9a6c4
jitheshk27 <jithesh.kumbla@qburst.com>
2025-03-17 16:56:21 +0530

    adding logic to handle multiple delimiters in add logic

--- services/string_calculator.rb
+++ services/string_calculator.rb
@@ -3,7 +3,14 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    
-    numbers.gsub("\n", ",").split(",").map(&:to_i).sum
+
+    delimiter = /,|\n/
+    if numbers.start_with?('//')
+      parts = numbers.split("\n", 2)
+      delimiter = Regexp.escape(parts[0][2..]) # can use delete_prefix or many more..
+      numbers = parts[1]
+    end
+
+    numbers.split(/#{delimiter}/).map(&:to_i).sum
   end
 end
