e07a5950a51495087493cce22b9fd66c64cba5e7
jitheshk27 <jithesh.kumbla@qburst.com>
2025-03-17 15:17:08 +0530

    fixing_rubocop_for_spec

--- spec/string_calulator_spec.rb
+++ spec/string_calulator_spec.rb
@@ -1,10 +1,16 @@
+# frozen_string_literal: true
+
 require 'rspec'
-require_relative "../services/string_calculator.rb"
+require_relative '../services/string_calculator'
 
 RSpec.describe ::StringCalculator do
-	describe "#add" do
-		it "should return 0 when an empty string is passed" do
-			expect(StringCalculator.new.add("")).to eq 0
-		end
-	end
+  describe '#add' do
+    it 'should return 0 when an empty string is passed' do
+      expect(StringCalculator.new.add('')).to eq 0
+    end
+
+    it 'should return the number itself when a single number is provided' do
+      expect(StringCalculator.new.add('1')).to eq 1
+    end
+  end
 end
