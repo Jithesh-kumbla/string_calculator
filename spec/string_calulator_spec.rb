58041deef2fc087e7eb4568dcb1ccec14b19d821
jitheshk27 <jithesh.kumbla@qburst.com>
2025-03-17 15:59:32 +0530

    using shared examples to refactor the test cases

--- spec/string_calulator_spec.rb
+++ spec/string_calulator_spec.rb
@@ -2,20 +2,26 @@
 
 require 'rspec'
 require_relative '../services/string_calculator'
+require 'support/shared_examples/string_calculator_shared_examples'
 
 RSpec.describe ::StringCalculator do
+  subject { described_class.new }
   describe '#add' do
-    it 'should return 0 when an empty string is passed' do
-      expect(StringCalculator.new.add('')).to eq 0
-    end
+    include_examples 'string calculator', '', 0
+    include_examples 'string calculator', '1', 1
+    include_examples 'string calculator', '1,2,3', 6
+    include_examples 'string calculator', '1,5', 6
+    # it 'should return 0 when an empty string is passed' do
+    #   expect(StringCalculator.new.add('')).to eq 0
+    # end
 
-    it 'should return the number itself when a single number is provided' do
-      expect(StringCalculator.new.add('1')).to eq 1
-    end
+    # it 'should return the number itself when a single number is provided' do
+    #   expect(StringCalculator.new.add('1')).to eq 1
+    # end
 
-    it 'should handle any amount of numbers that is passed as a string' do
-      expect(StringCalculator.new.add("1,2,3")).to eq 6
-      expect(StringCalculator.new.add("1,5")).to eq 6
-    end
+    # it 'should handle any amount of numbers that is passed as a string' do
+    #   expect(StringCalculator.new.add("1,2,3")).to eq 6
+    #   expect(StringCalculator.new.add("1,5")).to eq 6
+    # end
   end
 end
